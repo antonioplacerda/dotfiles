c7223e0cd3be83f7cab7d0a752dec72cf0be4f0a
antonioplacerda <antonioplacerda@gmail.com>
2021-02-07 21:52:10 +0000

    add aliases to python

---
+++ python/aliases.zsh
@@ -0,0 +1,10 @@
+alias py='python3'
+alias pym='py -m'
+
+# pip
+alias ppip='pym pip'
+alias pipi='pym pip install'
+alias pipir='pym pip install -r requirements.txt'
+alias pipr='pym pip freeze > requirements.txt'
+
+alias penv='pym venv'
