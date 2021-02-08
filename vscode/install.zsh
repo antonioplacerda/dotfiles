cf73f825a8aaac2ccc97ead62d532f1b78d93986
antonioplacerda <antonioplacerda@gmail.com>
2021-02-08 00:22:55 +0000

    [vscode] add settings and default keybindings

---
+++ vscode/install.zsh
@@ -0,0 +1,2 @@
+cp ~/.dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+cp ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
