a3d8b2d9b33836344333366cf0209e4f2faf5e8a
antonioplacerda <antonioplacerda@gmail.com>
2021-02-09 23:18:54 +0000

    [python] add pipu and penv aliases

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -4,7 +4,9 @@ alias pym='py -m'
 # pip
 alias ppip='pym pip'
 alias pipi='pym pip install'
+alias pipu='pym pip install --upgrade pip'
 alias pipir='pym pip install -r requirements.txt'
 alias pipr='pym pip freeze > requirements.txt'
 
-alias penv='pym venv'
+alias penv='pym venv .venv'
+alias penva='source ./.venv/bin/activate'
