f92978144e1593fabe399c9d6981125328336544
Antonio P Lacerda <antonioplacerda@gmail.com>
2023-06-12 22:05:13 +0100

    [nvim] Bind ^f to tmux-sessions

--- home/dot_config/nvim/lua/user/remap.lua
+++ home/dot_config/nvim/lua/user/remap.lua
@@ -42,3 +42,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessions<CR>")
