1bf92df756da85af03e2e208f9eb4563d00ca9bb
Antonio P Lacerda <antonioplacerda@gmail.com>
2024-05-01 13:47:42 +0100

    [zsh] python aliases

--- home/dot_config/zsh/aliases/python.zsh
+++ home/dot_config/zsh/aliases/python.zsh
@@ -1,13 +1,13 @@
-alias py='/usr/local/bin/python3'
-alias pym='/usr/local/bin/python3 -m'
+alias py='python3'
+alias pym='python3 -m'
 
 # pip
-alias ppip='/usr/local/bin/python3 -m pip'
-alias pipi='/usr/local/bin/python3 -m pip install'
-alias pipu='/usr/local/bin/python3 -m pip install --upgrade pip'
-alias pipir='/usr/local/bin/python3 -m pip install -r requirements.txt'
-alias pipr='/usr/local/bin/python3 -m pip freeze > requirements.txt'
+alias ppip='python3 -m pip'
+alias pipi='python3 -m pip install'
+alias pipu='python3 -m pip install --upgrade pip'
+alias pipir='python3 -m pip install -r requirements.txt'
+alias pipr='python3 -m pip freeze > requirements.txt'
 
 # venv
-alias penv='/usr/local/bin/python3 -m venv .venv'
+alias penv='python3 -m venv .venv'
 alias penva='source ./.venv/bin/activate'
